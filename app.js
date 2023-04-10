2980c0d65c9ec6d890421eb4f6edc9aa24b65803
DinnyTech <77016654+Danobentey@users.noreply.github.com>
2023-04-11 00:01:38 +0100

    added styling and dropdowns

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 
 
-const dropDown = document.querySelector(".dropDown");
-const dropElem = document.querySelector(".drop");
+const dropDown = document.querySelector("#supportDropdownButton");
+const currencyDrop = document.querySelector(".currency");
+const currencyDropdown = document.querySelector("#currency-drop");
+const dropElem = document.querySelector(".dropdown-menu");
+const accountElem = document.querySelector(".account");
+const accountDropdown = document.querySelector(".account-dropdown-menu");
+
 
 // dropdown/filter functionality
 dropDown.addEventListener("click", () => {
-  dropElem.classList.toggle("showDropDown");
-  console.log("Hello");
+  currencyDropdown.classList.remove("showCurrencyDropdown");
+  accountDropdown.classList.remove("showAccount");
+  dropElem.classList.toggle("showDropdown");
+});
+currencyDrop.addEventListener("click", () => {
+  dropElem.classList.remove("showDropdown");
+  accountDropdown.classList.remove("showAccount");
+  currencyDropdown.classList.toggle("showCurrencyDropdown");
+});
+accountElem.addEventListener("click", () => {
+  dropElem.classList.remove("showDropdown");
+  currencyDropdown.classList.remove("showCurrencyDropdown");
+  accountDropdown.classList.toggle("showAccount");
 });
 
 
