adc9600e0dc77c729b852a7beba743c1f6ab34cd
DinnyTech <77016654+Danobentey@users.noreply.github.com>
2023-04-10 23:27:43 +0100

    added navbar

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 
 
-const dropDown = document.querySelector(".dropDown");
-const dropElem = document.querySelector(".drop");
+const dropDown = document.querySelector("#supportDropdownButton");
+const currencyDrop = document.querySelector(".currency");
+const currencyDropdown = document.querySelector("#currency-drop");
+const dropElem = document.querySelector(".dropdown-menu");
+const accountElem = document.querySelector(".account");
+const accountDropdown = document.querySelector(".account-dropdown-menu");
+
 
 // dropdown/filter functionality
 dropDown.addEventListener("click", () => {
-  dropElem.classList.toggle("showDropDown");
+  currencyDropdown.classList.remove("showCurrencyDropdown");
+  accountDropdown.classList.remove("showAccount");
+  dropElem.classList.toggle("showDropdown");
+  console.log("Hello");
+});
+currencyDrop.addEventListener("click", () => {
+  dropElem.classList.remove("showDropdown");
+  accountDropdown.classList.remove("showAccount");
+  currencyDropdown.classList.toggle("showCurrencyDropdown");
+  console.log("Hello");
+});
+accountElem.addEventListener("click", () => {
+  dropElem.classList.remove("showDropdown");
+  currencyDropdown.classList.remove("showCurrencyDropdown");
+  accountDropdown.classList.toggle("showAccount");
   console.log("Hello");
 });
 
