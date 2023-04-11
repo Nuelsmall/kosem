597f7a6b849dca05dab288a6fd7039199a0bc227
Nuelsmall <nuelsmall30@gmail.com>
2023-04-11 10:58:25 +0100

    added add hotel section

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// "use strict"
 
 const dropDown = document.querySelector(".dropDown");
 const dropElem = document.querySelector(".drop");
@@ -9,21 +9,3 @@ dropDown.addEventListener("click", () => {
   console.log("Hello");
 });
 
-
-{/* <script type="text/javascript">
-        var referenceHeight = document.querySelector('.jumbotron').offsetHeight;
-        document.getElementsByTagName('body')[0].onscroll = function(){
-            let scrollTop = document.querySelector('.new-search-form').getBoundingClientRect().top
-    
-            if(scrollTop < 7)
-            {
-                document.querySelector('.search-form-container.container').classList.add('static');
-            }
-    
-            if(document.getElementsByTagName('html')[0].scrollTop < referenceHeight+50)
-            {
-                document.querySelector('.search-form-container.container').classList.remove('static');
-    
-            }
-        }
-    </script> */}
