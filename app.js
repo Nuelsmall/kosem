ed7a3b3f328a1bd77bcd159dc2ca42178477e2c8
DinnyTech <77016654+Danobentey@users.noreply.github.com>
2023-04-12 08:06:25 +0100

    added finishing touches to navbar

--- app.js
+++ app.js
@@ -13,16 +13,19 @@ dropDown.addEventListener("click", () => {
   currencyDropdown.classList.remove("showCurrencyDropdown");
   accountDropdown.classList.remove("showAccount");
   dropElem.classList.toggle("showDropdown");
+  console.log("Hello");
 });
 currencyDrop.addEventListener("click", () => {
   dropElem.classList.remove("showDropdown");
   accountDropdown.classList.remove("showAccount");
   currencyDropdown.classList.toggle("showCurrencyDropdown");
+  console.log("Hello");
 });
 accountElem.addEventListener("click", () => {
   dropElem.classList.remove("showDropdown");
   currencyDropdown.classList.remove("showCurrencyDropdown");
   accountDropdown.classList.toggle("showAccount");
+  console.log("Hello");
 });
 
 
