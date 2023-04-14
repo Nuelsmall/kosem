00fd7f50b259f9540ec453c5fea2005f8e41b2c4
Nuelsmall <nuelsmall30@gmail.com>
2023-04-14 23:18:54 +0100

    made final modifications

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 // "use strict"
 
-const dropDown = document.querySelector(".dropDown");
-const dropElem = document.querySelector(".drop");
 
-// dropdown/filter functionality
-dropDown.addEventListener("click", () => {
-  dropElem.classList.toggle("showDropDown");
-  console.log("Hello");
-});
 
